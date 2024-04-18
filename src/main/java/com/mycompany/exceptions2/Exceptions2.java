b8b6ea6deeb3fcea25424e12724e0618d390d59a
CamiiUribeee <acuribel@ufpso.edu.co>
2024-04-18 16:08:34 -0500

    modifications

--- src/main/java/com/mycompany/exceptions2/Exceptions2.java
+++ src/main/java/com/mycompany/exceptions2/Exceptions2.java
@@ -11,21 +11,41 @@ public class Exceptions2 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); 
-        int number; 
+        int number= readInteger("Escriba un numero: ");
+        System.out.println("El valor obtenido es: " + number);
         
-        try{
+        
+        /* do{
+            try{
             System.out.println("Ingrese un valor entero: ");
             number= scanner.nextInt(); 
             int square = number * number; 
-            System.out.println("El cuadrado de " + "" + number + " es: " + square); 
+            System.out.println("El cuadrado de " + "" + number + " es: " + square);  
+            break; 
         } catch(InputMismatchException ex){    //esto siempre se importa  
-            System.out.println("Debe ingresar valores numericos"); 
+            System.out.println("Debe ingresar valores numericos");    
         }
         
-        
-        
-        
-        
+        } while (true);  //revisar este do while */ 
+    }  
+    
+    public static int readInteger(String data){
+        boolean repeat; 
+        int value = 0; 
+        Scanner scanner = new Scanner(System.in); 
+        do {
+            repeat = false; 
+            try{
+                System.out.println(data); 
+                value = scanner.nextInt(); 
+            }catch (InputMismatchException e){
+                System.out.println("Tipo de dato no valido");
+                repeat = true; 
+            } finally{
+                System.out.println("Mensaje a mostrar siempre");
+            }
+        } while (repeat);
+        return value; 
     }
 }
 
